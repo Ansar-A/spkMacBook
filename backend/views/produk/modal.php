632afc320a05e671143af1d09dfca90d8a0008ef
Ansar-A <ansarkenshin@gmail.com>
2024-02-15 16:29:13 +0800

    test

--- backend/views/produk/modal.php
+++ backend/views/produk/modal.php
@@ -11,115 +11,18 @@ use yii\helpers\Url;
                         <div class="product-right-info">
                             <p>Website</p>
                             <h3><b>Sistem Pendukung Keputusan Penentuan MacBook Bekas</b></h3>
-                            <div class="rating">
-                                <ul class="list-inline">
-                                    <li><a class="fa fa-star" href=""></a></li>
-                                    <li><a class="fa fa-star" href=""></a></li>
-                                    <li><a class="fa fa-star" href=""></a></li>
-                                    <li><a class="fa fa-star" href=""></a></li>
-                                    <li><a class="fa fa-star-o" href=""></a></li>
-                                </ul>
-                            </div>
-
                             <h2><small class="text-muted">Unisversitas Islam Alauddin Makassar</small></h2>
                             <h5 class="m-t-20"><b>Ansar Kenshin</b> <span class="label label-default m-l-5">Developher</span></h5>
                             <hr>
                             <h5 class="font-600">Website Description</h5>
                             <p class="text-muted">Aplikasi Sistem Pendukung Keputusan (DSS) Penentuan MacBook Bekas Layak Pakai merupakan aplikasi yang dapat digunakan untuk menentukan MacBook bekas yang paling sesuai berdasarkan kualitas dengan tolak ukur budget yang disediakan.</p>
-                            <p class="text-muted">watch me:</p>
                             <div class="m-t-30">
-                                <p class="text-muted">Watch me at:</p>
-                                <button type="button" class="btn btn-youtube waves-effect waves-light">
-                                    <span class="btn-label"><i class="fa fa-youtube"></i> </span>Youtube
+                                <p class="text-muted">Look me at:</p>
+                                <button type="button" class="btn btn-github waves-effect waves-light">
+                                    <span class="btn-label"><i class="fa fa-github"></i> </span>Github
                                 </button>
                             </div>
                             <hr>
-                        </div>
-                    </div>
-                </div>
-                <div class="row m-t-30">
-                    <div class="col-xs-12">
-                        <h4><b>Info Data MacBook:</b></h4>
-                        <p class="text-muted">Unconsciously been working in
-                            his mind, or rather soul; for there are two distinct sorts of ideas,
-                            those that proceed from the head and those from the heart.</p>
-                        <div class="table-responsive m-t-20">
-                            <table class="table">
-                                <tbody>
-                                    <tr>
-                                        <td width="400">Brand</td>
-                                        <td>
-                                            TheBrandStore
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Color</td>
-                                        <td>
-                                            Black
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Length</td>
-                                        <td>
-                                            9 Centimeters
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Width</td>
-                                        <td>
-                                            20 Centimeters
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Height</td>
-                                        <td>
-                                            13 Centimeters
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Weight</td>
-                                        <td>
-                                            400 Grams
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Item part number:</td>
-                                        <td>
-                                            ABC2016
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Design</td>
-                                        <td>
-                                            Over-the-head
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Head Support</td>
-                                        <td>
-                                            No
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td width="400">Brand</td>
-                                        <td>
-                                            TheBrandStore
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Color</td>
-                                        <td>
-                                            Black
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Length</td>
-                                        <td>
-                                            9 Centimeters
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
                         </div>
                     </div>
                 </div>
