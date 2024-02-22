d07de6489f6206c3f3b6e9caaff966bd4c9d7b84
Ansar-A <ansarkenshin@gmail.com>
2024-02-22 08:28:54 +0800

    test

--- frontend/views/spk-kelayakan/_list_item.php
+++ frontend/views/spk-kelayakan/_list_item.php
@@ -5,7 +5,7 @@ use yii\helpers\Url;
 ?>
 <div class="portfolio-item col-lg-4 col-sm-2 mockups">
     <div class="he-wrap tpl6 market-item">
-        <img class="lazyOwl" alt="" src="<?= Url::to('@web/' . $model->produk->photo) ?>" style="display: block; opacity: 1; transition-property: all; transition-duration: 0s; transition-timing-function: ease; max-width: 100%; height: auto;">
+        <img class="lazyOwl" alt="" src="<?= Url::to('@web/' . $model->produk->photo) ?>" style="display: block; opacity: 1; transition-property: all; transition-duration: 0s; transition-timing-function: ease; max-width: 100%; height: auto; border-radius: 10px;">
         <div class="he-view">
             <div class="bg a0" data-animate="fadeIn">
                 <h3 class="a1" data-animate="fadeInDown">
